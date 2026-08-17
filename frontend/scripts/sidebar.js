c331273434b7b53ad364b3f7e54b5813d5d2a86d
Ritik Thakur <188522288+ritikthakur22@users.noreply.github.com>
2026-08-17 16:51:23 +0545

    Update version to 1.0.2 and fix Privacy Policy links

--- frontend/scripts/sidebar.js
+++ frontend/scripts/sidebar.js
@@ -143,11 +143,11 @@ document.addEventListener('DOMContentLoaded', () => {
                 <div class="sidebar-item-desc" style="line-height: 1.5; font-size: 14px;">
                     <b>Name:</b> FastShare<br>
                     <b>Package:</b> com.crdy.fastshare<br>
-                    <b>Version:</b> 1.0.1<br>
+                    <b>Version:</b> 1.0.2<br>
                     <b>Contact:</b> <a href="mailto:ritikthakur@duck.com" style="color: var(--sidebar-primary);">ritikthakur@duck.com</a><br>
                     <b>GitHub:</b> <a href="https://github.com/ritikthakur22/FastShare" target="_blank" style="color: var(--sidebar-primary);">ritikthakur22/FastShare</a><br>
                     <b>Portfolio:</b> <a href="https://ritikthakur.com.np" target="_blank" style="color: var(--sidebar-primary);">ritikthakur.com.np</a><br>
-                    <a href="#" style="color: var(--sidebar-primary);">Privacy Policy</a> | <a href="#" style="color: var(--sidebar-primary);">Terms of Use</a>
+                    <a href="https://docs.google.com/document/d/1oFZcr9ay7BewS7WNaIM0HNQDbZHaV-uLAxA7LTQDOIQ/edit?usp=sharing" target="_blank" style="color: var(--sidebar-primary);">Privacy Policy</a> | <a href="https://docs.google.com/document/d/1Wv_7CyEoj02Tlq5ygovSfIkPNRyaaUTh8cdrSaS7rtk/edit?usp=sharing" target="_blank" style="color: var(--sidebar-primary);">Terms of Use</a>
                 </div>
             </div>
         </div>
